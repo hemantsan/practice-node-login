b4650da1a80dd76e6a6a6925353b3456d2d34056
Viecky <sankhlahemant251@gmail.com>
2023-01-24 21:01:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render('pages/index');
+});
+
+app.get('/login', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render('pages/login');
+});
+
+app.post('/login', function (req, res) {
+  if (req.body.username === 'admin' && req.body.password === 'admin') {
+    res.render('pages/dashboard', { success: true });
+  } else {
+    res.render('pages/login', { success: false });
+  }
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
